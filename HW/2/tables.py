7c38a6faa640574058dec5740dbcfb9265fa8f30
Joseph Robbins <jrobbins6956@sohamcollege.org.uk>
2014-07-09 16:10:05 +0000

    Update tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,15 @@
 #paste code here
+name = input("What is your name >>>") #asking user for name and assigning it to 'name'
+
+print("Do you know your times table?")
+print()
+
+number = int(input("Please enter a number >>>")) #asking user for a number and assigning it to 'number'
+stop = int(input("Please enter the amount of times table you want displayed as an integer >>>")) #asking user for a number and assigning it to 'stop' 
+print()
+
+
+for table in range(stop+1): #repeat same number of times as repeats requested add one
+    print("{0} times {1} = {2}".format(table,number,table*number)) #printing text with times table calculation
+
+print("Now you know your times table {0}".format(name))#message to user  
